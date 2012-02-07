14fe3fee3987bb08ecdfd5b8c0db7af57ee1da38
Art.py <lekcjephp@gmail.com>
2012-02-07 12:46:46 +0000

    wprowadzenie przedzialu dla lat

--- main.py
+++ main.py
@@ -6,8 +6,19 @@ print (c)
 """
 Napisz program, ktory wyswietla pelny kalendarz roku podanego przez uzytkownika
 """
-
+"""
 year = int (input("Please entare a year \n"))
 for miesiac in range(1,13):
 	c=calendar.month(year, miesiac)
 	print(c)
+====================================================================================
+	uzywtkowinik wpisuje jeden rok i drugi rok i program ma wypisac kalendarze dla 
+	wszystkich lat w tym przedziale
+"""	
+year1 = int (input("Please, enter a 1st year"))
+year2 = int (input("Please, enter a 2nd year"))
+
+for rok in range(year1, year2+1):
+	for miesiac in range(1,13):
+		c=calendar.month(rok, miesiac)
+		print(c)
