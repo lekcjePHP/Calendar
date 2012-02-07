1df269fc6ad5a075a800b4d2229a5869db4eaabe
Art.py <lekcjephp@gmail.com>
2012-02-07 12:28:40 +0000

    dodanie funkcji ladujacej kalendarz na wpisany rok

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 import calendar 
+"""
 c=calendar.month(2012,2)
 print (c)
+"""
+"""
+Napisz program, ktory wyswietla pelny kalendarz roku podanego przez uzytkownika
+"""
+
+year = int (input("Please entare a year \n"))
+for miesiac in range(1,13):
+	c=calendar.month(year, miesiac)
+	print(c)
